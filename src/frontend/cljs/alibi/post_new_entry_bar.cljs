a8a039adc0f4127300ae1e92a94afbfc15beac62
Freek Paans <freekpaans@gmail.com>
2017-04-05 16:48:07 +0200

    entry bar to om

--- src/frontend/cljs/alibi/post_new_entry_bar.cljs
+++ src/frontend/cljs/alibi/post_new_entry_bar.cljs
@@ -43,250 +43,78 @@
 
 (defn entry-bar-form' [for-state owner]
   (log "data %o" for-state)
-  (reify
-    om/IDidMount
-    (did-mount [_]
-      (let [$form (js/$ (om/get-node owner "the-form"))
-            $select (.find $form "select")]
-        (.selectize
-          $select #js
-          {:highlight false
-           :selectOnTab true
-           :score (partial selectize-score-fn for-state)
-           :render #js {:option (partial selectize-render-option
-                                         (:options-by-id for-state))
-                        :item (fn [i e] (selectize-render-option
-                                          (:options-by-id for-state)
-                                          i e "option-selected"))}})
-        (let [selectize (aget $select 0 "selectize")]
-          (.on selectize "dropdown_close"
-               (fn []
-                 (let [value (.getValue selectize)]
-                   (.setTimeout ; updating the state will trigger a destroy
-                                ; of selectize, which in turn will prevent
-                                ; the dropdown_close event handlers from
-                                ; complete correctly, so we schedule
-                                ; updating the state for a later moment
-                                js/window
-                                (fn [] (if (seq value)
-                                         ((:on-select-task for-state)
-                                          (parse-selected-item value))
-                                         ((:on-cancel for-state))))
-                                0)))))))
-    om/IRender
-    (render [_]
-      (let [options (:options for-state)
-            {:keys [projectId taskId]} (:selected-item for-state)
-            select-value (if (and projectId taskId)
-                           (str projectId "," taskId) "")]
-        (dom/form
-          #js {:id "post-new-entry-bar"
-               :className "navbar-form navbar-form-post-new-entry-bar"
-               :ref "the-form"}
-          (dom/select
-            #js {:name "post-new-entry-bar"
-                 :placeholder "Post new entry..."
-                 :defaultValue select-value}
-            (dom/option #js {:value ""} "")
-            (map #(let [value (str (:project-id %) "," (:task-id %))]
-                    (dom/option
-                      #js {:key value
-                           :data-project-id (:project-id %)
-                           :data-task-id (:task-id %)
-                           :data-billing-method (:billing-method %)
-                           :value value}
-                      (:text %))) options)))))))
-  ;(letfn [(get-selectize [this] (.. (js/$ (aget this "form"))
-                                     ;(find "select")
-                                     ;(get 0)
-                                     ;-selectize))]
-    ;(. js/React createClass
-      ;#js
-      ;{:componentWillUnmount
-       ;(fn []
-         ;(this-as
-           ;this
-           ;;(log "will unmount")
-           ;(.destroy (get-selectize this))))
+  (letfn [(get-selectize [] (.. (js/$ (om/get-node owner "the-form"))
+                              (find "select")
+                              (get 0)
+                              -selectize))]
+    (reify
+      om/IDidMount
+      (did-mount [_]
+        (let [$form (js/$ (om/get-node owner "the-form"))
+              $select (.find $form "select")]
+          (.selectize
+            $select #js
+            {:highlight false
+             :selectOnTab true
+             :score (partial selectize-score-fn for-state)
+             :render #js {:option (partial selectize-render-option
+                                           (:options-by-id for-state))
+                          :item (fn [i e] (selectize-render-option
+                                            (:options-by-id for-state)
+                                            i e "option-selected"))}})
+          (let [selectize (aget $select 0 "selectize")]
+            (.on selectize "dropdown_close"
+                 (fn []
+                   (let [value (.getValue selectize)]
+                     (.setTimeout ; updating the state will trigger a destroy
+                                  ; of selectize, which in turn will prevent
+                                  ; the dropdown_close event handlers from
+                                  ; complete correctly, so we schedule
+                                  ; updating the state for a later moment
+                                  js/window
+                                  (fn [] (if (seq value)
+                                           ((:on-select-task for-state)
+                                            (parse-selected-item value))
+                                           ((:on-cancel for-state))))
+                                  0)))))))
+      om/IWillUnmount
+      (will-unmount [_]
+        ;(log "will unmount")
+        (.destroy (get-selectize)))
 
-       ;:componentDidUpdate
-       ;(fn []
-         ;(this-as
-           ;this
-           ;(let [for-state (aget this "props" "clj-props")
-                 ;{:keys [projectId taskId]} (:selected-item for-state)
-                 ;selectize (get-selectize this)
-                 ;current-val (.getValue selectize)]
-             ;(if (and projectId taskId)
-               ;(let [new-val (str projectId "," taskId)]
-                 ;(when-not (= new-val current-val)
-                   ;(.clear selectize)
-                   ;(.addItem selectize new-val)))
-               ;(.clear selectize)))))
+      om/IDidUpdate
+      (did-update [_ _ _]
+        (let [{:keys [projectId taskId]} (:selected-item for-state)
+              selectize (get-selectize)
+              current-val (.getValue selectize)]
+          (if (and projectId taskId)
+            (let [new-val (str projectId "," taskId)]
+              (when-not (= new-val current-val)
+                (.clear selectize)
+                (.addItem selectize new-val)))
+            (.clear selectize))))
 
-       ;:componentDidMount
-       ;(fn []
-         ;(this-as
-           ;this
-           ;(let [for-state (aget this "props" "clj-props")
-                 ;$form (js/$ (aget this "form"))
-                 ;$select (.find $form "select")]
-             ;(.selectize
-               ;$select #js
-               ;{:highlight false
-                ;:selectOnTab true
-                ;:score (partial selectize-score-fn for-state)
-                ;:render #js {:option (partial selectize-render-option
-                                              ;(:options-by-id for-state))
-                             ;:item (fn [i e] (selectize-render-option
-                                               ;(:options-by-id for-state)
-                                               ;i e "option-selected"))}})
-             ;(let [selectize (aget $select 0 "selectize")]
-               ;(.on selectize "dropdown_close"
-                    ;(fn []
-                      ;(let [value (.getValue selectize)]
-                        ;(.setTimeout ; updating the state will trigger a destroy
-                                     ;; of selectize, which in turn will prevent
-                                     ;; the dropdown_close event handlers from
-                                     ;; complete correctly, so we schedule
-                                     ;; updating the state for a later moment
-                                     ;js/window
-                                     ;(fn [] (if (seq value)
-                                             ;((:on-select-task for-state)
-                                              ;(parse-selected-item value))
-                                             ;((:on-cancel for-state))))
-                                     ;0))))))))
-
-       ;:shouldComponentUpdate
-       ;(fn [next-props]
-         ;(this-as
-           ;this
-           ;(let [next-state (aget next-props "clj-props")
-                 ;this-state (aget this "props" "clj-props")]
-             ;(not= (:selected-item next-state) (:selected-item this-state)))))
-
-       ;:render
-       ;(fn []
-         ;(this-as
-           ;this
-           ;(let [for-state (aget this "props" "clj-props")
-                 ;options (:options for-state)
-                 ;{:keys [projectId taskId]} (:selected-item for-state)
-                 ;select-value (if (and projectId taskId)
-                                ;(str projectId "," taskId) "")]
-             ;(comment (sab/html
-               ;[:form#post-new-entry-bar.navbar-form.navbar-form-post-new-entry-bar
-                ;{:ref (fn [form] (aset this "form" form))}
-                ;[:select
-                 ;{:name "post-new-entry-bar"
-                  ;:placeholder "Post new entry..."
-                  ;:defaultValue select-value}
-                 ;[:option
-                  ;{:value ""}
-                  ;""]
-                 ;(map #(let [value (str (:project-id %) "," (:task-id %))]
-                         ;[:option
-                          ;{:key value
-                           ;:data-project-id (:project-id %)
-                           ;:data-task-id (:task-id %)
-                           ;:data-billing-method (:billing-method %)
-                           ;:value value}
-                          ;(:text %)]) options)]])))))})))
-
-(def entry-bar-form
-  (letfn [(get-selectize [this] (.. (js/$ (aget this "form"))
-                                     (find "select")
-                                     (get 0)
-                                     -selectize))]
-    (. js/React createClass
-      #js
-      {:componentWillUnmount
-       (fn []
-         (this-as
-           this
-           ;(log "will unmount")
-           (.destroy (get-selectize this))))
-
-       :componentDidUpdate
-       (fn []
-         (this-as
-           this
-           (let [for-state (aget this "props" "clj-props")
-                 {:keys [projectId taskId]} (:selected-item for-state)
-                 selectize (get-selectize this)
-                 current-val (.getValue selectize)]
-             (if (and projectId taskId)
-               (let [new-val (str projectId "," taskId)]
-                 (when-not (= new-val current-val)
-                   (.clear selectize)
-                   (.addItem selectize new-val)))
-               (.clear selectize)))))
-
-       :componentDidMount
-       (fn []
-         (this-as
-           this
-           (let [for-state (aget this "props" "clj-props")
-                 $form (js/$ (aget this "form"))
-                 $select (.find $form "select")]
-             (.selectize
-               $select #js
-               {:highlight false
-                :selectOnTab true
-                :score (partial selectize-score-fn for-state)
-                :render #js {:option (partial selectize-render-option
-                                              (:options-by-id for-state))
-                             :item (fn [i e] (selectize-render-option
-                                               (:options-by-id for-state)
-                                               i e "option-selected"))}})
-             (let [selectize (aget $select 0 "selectize")]
-               (.on selectize "dropdown_close"
-                    (fn []
-                      (let [value (.getValue selectize)]
-                        (.setTimeout ; updating the state will trigger a destroy
-                                     ; of selectize, which in turn will prevent
-                                     ; the dropdown_close event handlers from
-                                     ; complete correctly, so we schedule
-                                     ; updating the state for a later moment
-                                     js/window
-                                     (fn [] (if (seq value)
-                                             ((:on-select-task for-state)
-                                              (parse-selected-item value))
-                                             ((:on-cancel for-state))))
-                                     0))))))))
-
-       :shouldComponentUpdate
-       (fn [next-props]
-         (this-as
-           this
-           (let [next-state (aget next-props "clj-props")
-                 this-state (aget this "props" "clj-props")]
-             (not= (:selected-item next-state) (:selected-item this-state)))))
-
-       :render
-       (fn []
-         (this-as
-           this
-           (let [for-state (aget this "props" "clj-props")
-                 options (:options for-state)
-                 {:keys [projectId taskId]} (:selected-item for-state)
-                 select-value (if (and projectId taskId)
-                                (str projectId "," taskId) "")]
-             (comment (sab/html
-               [:form#post-new-entry-bar.navbar-form.navbar-form-post-new-entry-bar
-                {:ref (fn [form] (aset this "form" form))}
-                [:select
-                 {:name "post-new-entry-bar"
-                  :placeholder "Post new entry..."
-                  :defaultValue select-value}
-                 [:option
-                  {:value ""}
-                  ""]
-                 (map #(let [value (str (:project-id %) "," (:task-id %))]
-                         [:option
-                          {:key value
-                           :data-project-id (:project-id %)
-                           :data-task-id (:task-id %)
-                           :data-billing-method (:billing-method %)
-                           :value value}
-                          (:text %)]) options)]])))))})))
+      om/IRender
+      (render [_]
+        ;(log "rerendering")
+        (let [options (:options for-state)
+              {:keys [projectId taskId]} (:selected-item for-state)
+              select-value (if (and projectId taskId)
+                             (str projectId "," taskId) "")]
+          (dom/form
+            #js {:id "post-new-entry-bar"
+                 :className "navbar-form navbar-form-post-new-entry-bar"
+                 :ref "the-form"}
+            (dom/select
+              #js {:name "post-new-entry-bar"
+                   :placeholder "Post new entry..."
+                   :defaultValue select-value}
+              (dom/option #js {:value ""} "")
+              (map #(let [value (str (:project-id %) "," (:task-id %))]
+                      (dom/option
+                        #js {:key value
+                             :data-project-id (:project-id %)
+                             :data-task-id (:task-id %)
+                             :data-billing-method (:billing-method %)
+                             :value value}
+                        (:text %))) options))))))))
